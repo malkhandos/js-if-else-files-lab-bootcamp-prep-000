7cb014749e490fe658ad244eb53c764f2ddb0351
Rommy Elyan <relyan@email.arizona.edu>
2018-03-27 19:01:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ var height = "74"
 if (name === 'Susan') {
 console.log('The name is correct')
 }
-else {
-  console.log('The name is incorrect')
-}
+
 // Don't worry about this
 module.exports = { name, height
 }
